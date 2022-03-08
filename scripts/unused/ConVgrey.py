1a5071b41c1dfe8fa051a297c21308e6c13ba153
Szymon <szymonhryniewski53@gmail.com>
2022-03-08 14:25:16 +0100

    poprawa struktury projektu

--- scripts/unused/ConVgrey.py
+++ scripts/unused/ConVgrey.py
@@ -7,7 +7,7 @@ import os
 
 # model 2
 model = Sequential([
-    Conv2D(100, (3, 3), activation='relu', input_shape=(150, 100, 3)),
+    Conv2D(100, (3, 3), activation='relu', input_shape=(150, 100, 1)),
     MaxPooling2D(2, 2),
 
     Conv2D(100, (3, 3), activation='relu'),
@@ -23,35 +23,36 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
 
 
 path=os.getcwd()
-path=os.path.join(path, 'dataset')
+path=os.path.join(path,'dataset')
 
 # declare train_path
-path_train = os.path.join(path, 'Train')
+path_train=os.path.join(path,'Train')
 
 # declare test_path
-path_test = os.path.join(path, 'Test')
-train_datagen = ImageDataGenerator(rescale=1.0 / 255)
+path_test=os.path.join(path,'Test')
+train_datagen=ImageDataGenerator(rescale=1.0 / 255)
 
 train_generator = train_datagen.flow_from_directory(path_train,
                                                     batch_size=10,
+                                                    color_mode="grayscale",
                                                     target_size=(150, 100))
 
 validation_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
 validation_generator = validation_datagen.flow_from_directory(path_test,
                                                               batch_size=10,
+                                                              color_mode="grayscale",
                                                               target_size=(150, 100))
 
 history = model.fit_generator(train_generator,
-                              epochs=3,
-                              validation_data=validation_generator,
-                              )
+                              epochs=6,
+                              validation_data=validation_generator)
 
 
-
-img = next(train_generator)[0][0].reshape(1, 150, 100, 3)
+img = next(train_generator)[0][0].reshape(1, 150, 100)
 model.predict(img)
 model.predict(next(train_generator)[0])
 model.predict(next(train_generator)[0])
 
-model.save("Conv.h5")
+model.save("Conv_nocol.h5")
+
